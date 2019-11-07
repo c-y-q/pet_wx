2fb2362fc8547fb5e6d073e00f4cc17fec7eaf22
eggsy.cao <eggsy.cao@pictureworks.biz>
2019-11-07 15:40:16 +0800

    身份证号码强验证

--- routes/petRegister.js
+++ routes/petRegister.js
@@ -8,20 +8,14 @@ const axios = require('axios');
 function regIdCard(idcode) {
   const weight_factor = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2];
   const check_code = ['1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2'];
-  const code = idcode + "";
-  let last = idcode[17]; //最后一位
-  const seventeen = code.substring(0, 17);
-  const arr = seventeen.split("");
-  const len = arr.length;
-  let num = 0;
-  for (let i = 0; i < len; i++) {
-    num = num + arr[i] * weight_factor[i];
-  }
-  const resisue = num % 11;
-  const last_no = check_code[resisue];
   const idcard_patter = /^[1-9][0-9]{5}([1][9][0-9]{2}|[2][0][0|1][0-9])([0][1-9]|[1][0|1|2])([0][1-9]|[1|2][0-9]|[3][0|1])[0-9]{3}([0-9]|[X])$/;
   const format = idcard_patter.test(idcode);
-  return last === last_no && format ? true : false;
+  const seventeen = idcode.substring(0, 17);
+  let num = 0;
+  for (let i = 0; i < seventeen.length; i++) {
+    num = num + seventeen[i] * weight_factor[i];
+  }
+  return idcode[17] === check_code[num % 11] && format ? true : false;
 }
 
 const regPhoneNum = /(^1[3456789]\d{9}$)|(^(0\d{2,3}\-)?([2-9]\d{6,7})+(\-\d{1,6})?$)/;
