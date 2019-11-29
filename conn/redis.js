a601258a04b62445a9e986c68c13d8d96bcb7668
xiaomi233 <xiaomi23333@126.com>
2019-11-29 11:12:38 +0800

    更改redis集群

--- conn/redis.js
+++ conn/redis.js
@@ -1,5 +1,6 @@
 const config = require("../config/config");
 const Redis = require('ioredis');
+// const cache = new Redis.Cluster(config.redis.config);
 const cache = new Redis(config.redis.config);
 module.exports = {
     cache: cache,
