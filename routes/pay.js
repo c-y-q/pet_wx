50d64dbe39c58e466568ca99e079f76b18789f15
xiaomi233 <xiaomi23333@126.com>
2019-12-09 10:36:09 +0800

    小程序缴费

--- routes/pay.js
+++ routes/pay.js
@@ -6,6 +6,7 @@ const moment = require('moment');
 const service = require('../service/pay');
 const registerService = require('../service/petRegister');
 const axios = require('axios');
+const md5 = require('md5-hex');
 const {
     cache,
     reqCount,
@@ -52,13 +53,14 @@ router.post('/wpPay', async (req, res) => {
             respMsg: " order is not exists !"
         }
     }
+
     //3.支付
     const {
-        Authorization,
+        // Authorization,
         mid,
         merchantUserId,
         tid
-    } = config.wppay;
+    } = config.pay;
     const params = {
         msgId: uuidTool().replace(/-/gi, ''),
         requestTimestamp: moment().format('YYYY-MM-DD HH:mm:ss'),
@@ -68,48 +70,60 @@ router.post('/wpPay', async (req, res) => {
         instMid: "MINIDEFAULT",
         totalAmount: orderInfo[0].total_price * 100, //单位为分
         subOpenId: openid,
+        openid: openid,
         tradeType: "MINI",
         merchantUserId: merchantUserId,
-        msgType: "WXPay",
+        msgType: "wx.unifiedOrder",
         msgSrc: "WWW.TEST.COM",
         msgSrcId: "3194",
         notifyUrl: "http://pet.hbzner.com/wx/wpPayNotify"
     }
 
+    let rest = {};
+    const keyArray = Object.keys(params).sort();
+    for (const key of keyArray) {
+        rest[key] = params[key];
+    }
+
+    /**
+    * 输入排序过后的key=value 值数组,用  "&" 字符拼接为字符串
+    */
+    let longStr = '';
+    for (const str in rest) {
+        longStr += str + '=' + rest[str] + '&';
+    }
+    console.log('---待签名参数---', longStr);
+    let signs = md5(longStr.substring(0, longStr.length - 1)+'fcAmtnx7MwismjWNhNKdHC44mNXtnEQeJkRrhKJwyrW2ysRR');// 移除最后一个 & 符号 生成签名
+    params.sign = signs;
+    // const sign = longStr + 'sign=' + signs;
+    // console.log(117, sign);
+
     const requestOptins = {
         method: 'POST',
         headers: {
-            'Authorization': Authorization
+            // 'Authorization': Authorization
+        'Content-Type': 'application/x-www-form-urlencoded',
         },
         data: params,
-        url: 'http://58.247.0.18:29015/v1/netpay/wx/unified-order' //https://api-mop.chinaums.com/v1/netpay/wx/unified-order
-    }
-    const {
-        data: {
-            targetSys,
-            status,
-            errCode,
-            totalAmount,
-            targetStatus,
-            qrCode,
-            miniPayRequest,
-            merOrderId
+        url: 'https://qr-test2.chinaums.com/netpay-route-server/api/' //https://api-mop.chinaums.com/v1/netpay/wx/unified-order
+    }// http://58.247.0.18:29015/v1/netpay/wx/unified-order
+    const data = await axios(requestOptins);
+    console.log(120, data);
+        if (data.data.status != 'WAIT_BUYER_PAY' || data.data.errCode != 'SUCCESS' || data.data.targetStatus != "SUCCESS|SUCCESS" || data.data.targetSys != 'WXPay') {
+            throw {
+                status: 400,
+                msg: 'order is not right!'
+            }
         }
-    } = await axios(requestOptins);
-    if (status != 'WAIT_BUYER_PAY' || errCode != 'SUCCESS' || targetStatus != "SUCCESS|SUCCESS" || targetSys != 'WXPay') {
-        throw {
-            status: 400,
-            msg: 'order is not right!'
+        const resData = {
+            appId: data.data.miniPayRequest.appid,
+            nonceStr: data.data.miniPayRequest.noncestr,
+            package: data.data.miniPayRequest.package,
+            signType: data.data.miniPayRequest.signType,
+            timeStamp: data.data.miniPayRequest.timeStamp,
+            paySign: data.data.miniPayRequest.paySign,
         }
-    }
-    const resData = {
-        appId: miniPayRequest.appid,
-        nonceStr: miniPayRequest.noncestr,
-        package: `prepay_id=${miniPayRequest.prepayid}`,
-        signType: 'MD5',
-        timeStamp: parseInt(Date.now() / 1000).toString(),
-        paySign: miniPayRequest.sign
-    }
+
     res.json({
         status: 200,
         result: resData
