e29c39674bf298b69cb0785391b8b6af0d7b27ee
eggsy.cao <eggsy.cao@pictureworks.biz>
2019-12-26 10:10:02 +0800

    小程序提交年审，新登记旧证升级，审核状态发生变化，微信公众号给用户发送通知

--- config/config.js
+++ config/config.js
@@ -39,14 +39,7 @@ module.exports = {
             queueLimit: 0 //连接池的最大请求数，从getConnection方法前依次排队。设置为0将没有限制
         }
     },
-    wxpublic: {
-        appid: 'wx833268732e68920c',
-        appsecret: '92d5f8b18485998950486d25a41ca14c',
-        access_token: '',
-        token: '123456',
-        encodingAESKey: 'olz6qa2Ew77LFRB7GVGoHraPeGvVBX3SerO0ehVIjFp',
-        checkSignature: true // 可选，默认为true。由于微信公众平台接口调试工具在明文模式下不发送签名，所以如要使用该测试工具，请将其设置为false
-    },
+
     // redis: {
     //     config: [{
     //         port: 6379,
